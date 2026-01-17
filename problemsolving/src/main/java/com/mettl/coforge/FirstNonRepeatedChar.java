32f73b3ec3309374fa60d494df91462dd236a6c6
manishkumarmdb <manishkumarmdb@gmail.com>
2026-01-16 23:45:58 -0600

    practiced to write another function to find first non repeated char from given string

--- problemsolving/src/main/java/com/mettl/coforge/FirstNonRepeatedChar.java
+++ problemsolving/src/main/java/com/mettl/coforge/FirstNonRepeatedChar.java
@@ -3,6 +3,8 @@ package com.mettl.coforge;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
+// Find the first non-repeated character in a given string
+// Input string contains only lowercase letters
 public class FirstNonRepeatedChar {
 
   private static String firstNonRepeatedChar(String str) {
@@ -21,8 +23,22 @@ public class FirstNonRepeatedChar {
     return result;
   }
 
+  private static String firstNonRepeatedChar_II(String str) {
+    int[] C = new int[26];
+    for (char ch : str.toCharArray()) {
+      C[ch - 'a'] = C[ch - 'a'] + 1;
+    }
+
+    for (char ch : str.toCharArray()) {
+      if (C[ch - 'a'] == 1)
+        return String.valueOf(ch);
+    }
+    return "";
+  }
+
   public static void main(String[] args) {
-    String str = "manish kumar";
+    String str = "mannishkumar";
     System.out.println(firstNonRepeatedChar(str));
+    System.out.println(firstNonRepeatedChar_II(str));
   }
 }
