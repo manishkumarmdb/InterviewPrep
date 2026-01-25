b0d967e3242735c41295877ad1a30be375b1f38a
manishkumarmdb <manishkumarmdb@gmail.com>
2026-01-24 21:56:57 -0600

    Added detailed explanation comments

--- problemsolving/src/main/java/com/leetcode/LeetCode0724FindPivotIndex.java
+++ problemsolving/src/main/java/com/leetcode/LeetCode0724FindPivotIndex.java
@@ -1,17 +1,34 @@
 package com.leetcode;
 
 public class LeetCode0724FindPivotIndex {
-    public int pivotIndex(int[] nums) {
-        int totalSum = 0, leftSum = 0, rightSum = 0;
-        for (int num : nums)
-            totalSum += num;
-        int i = 0, l = 0, r = nums.length - 1;
-        while (i < nums.length) {
-            rightSum = totalSum - nums[i] - leftSum;
-            if (leftSum == rightSum)
-                return i;
-            leftSum += nums[i++];
-        }
-        return -1;
+  // Prefix-Suffix Sum approach
+  // Time: O(n), Space: O(1)
+  // Find pivot index where sum of left elements = sum of right elements
+  // Pivot is not included in either sum comparison
+  // Example: [1,7,3,6,5,6] → pivot at index 3 (left sum=11, right sum=11)
+  public int pivotIndex(int[] nums) {
+    // Calculate total sum of all elements
+    int totalSum = 0;
+    for (int num : nums)
+      totalSum += num;
+    
+    int leftSum = 0;   // Running sum of elements to the left of current index
+    
+    // Check each index as potential pivot
+    for (int i = 0; i < nums.length; i++) {
+      // Calculate right sum: total - current element - left sum
+      // rightSum = sum of all elements to the right of index i
+      int rightSum = totalSum - nums[i] - leftSum;
+      
+      // If left sum equals right sum, found the pivot
+      if (leftSum == rightSum)
+        return i;
+      
+      // Add current element to left sum for next iteration
+      leftSum += nums[i];
     }
+    
+    // No pivot found
+    return -1;
+  }
 }
