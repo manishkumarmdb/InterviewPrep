9903a869177b43986fa87a6de25e018d5e6918e6
manishkumarmdb <manishkumarmdb@gmail.com>
2026-02-08 14:42:04 -0600

    revised on find all possible combination of 0 and 1

--- problemsolving/src/main/java/interview/asked/BinaryStrings.java
+++ problemsolving/src/main/java/interview/asked/BinaryStrings.java
@@ -2,24 +2,28 @@ package interview.asked;
 
 import java.util.Arrays;
 
+/*
+ * Write a program to generate and print all binary strings of length n.
+ * or
+ * Given an integer n, print all possible combinations of 0 and 1 of length n.
+ * */
 public class BinaryStrings {
 
-    public void generateAllBinaryString(int n, int[] A, int index) {
-        if (index == n) {
-            System.out.println(Arrays.toString(A));
-            return;
-        }
-        A[index] = 0;
-        generateAllBinaryString(n, A, index + 1);
-        //System.out.println(Arrays.toString(A));
-        A[index] = 1;
-        generateAllBinaryString(n, A, index + 1);
+  private static void generateAllBinaryString(int n, int[] A, int index) {
+    if (index == n) {
+      System.out.println(Arrays.toString(A));
+      return;
     }
+    A[index] = 0;
+    generateAllBinaryString(n, A, index + 1);
 
-    public static void main(String[] args) {
-        BinaryStrings obj = new BinaryStrings();
-        int n = 4;
-        int[] A = new int[n];
-        obj.generateAllBinaryString(n, A, 0);
-    }
+    A[index] = 1;
+    generateAllBinaryString(n, A, index + 1);
+  }
+
+  public static void main(String[] args) {
+    int n = 3;
+    int[] A = new int[n];
+    generateAllBinaryString(n, A, 0);
+  }
 }
