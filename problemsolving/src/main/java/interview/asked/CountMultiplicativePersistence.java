0a90f08f1627a1a03ab81ee1ada1f4ca2c082ce1
manishkumarmdb <manishkumarmdb@gmail.com>
2026-02-09 22:14:09 -0600

    revised to count multiplicative persistence

--- problemsolving/src/main/java/interview/asked/CountMultiplicativePersistence.java
+++ problemsolving/src/main/java/interview/asked/CountMultiplicativePersistence.java
@@ -2,33 +2,33 @@ package interview.asked;
 
 public class CountMultiplicativePersistence {
 
-    public static int persistence(long num) {
-        if (num < 10) {
-            return 0;
-        }
-
-        int count = 0;
-        System.out.println("number : " + num);
-        while (num > 9) {
-            num = digitsProduct(num);
-            System.out.println("digitsProduct : " + num);
-            count++;
-        }
-
-        return count;
+  public static int persistence(long num) {
+    if (num < 10) {
+      return 0;
     }
 
-    private static long digitsProduct(long num) {
-        long product = 1;
-        while (num > 0) {
-            product *= num % 10;
-            num /= 10;
-        }
-        return product;
+    int count = 0;
+    System.out.println("number : " + num);
+    while (num > 9) {
+      num = digitsProduct(num);
+      System.out.println("digitsProduct : " + num);
+      count++;
     }
 
-    public static void main(String[] args) {
-        int input = 39;
-        System.out.println(persistence(input));
+    return count;
+  }
+
+  private static long digitsProduct(long num) {
+    long product = 1;
+    while (num > 0) {
+      product *= num % 10;
+      num /= 10;
     }
+    return product;
+  }
+
+  public static void main(String[] args) {
+    int input = 39;
+    System.out.println(persistence(input));
+  }
 }
