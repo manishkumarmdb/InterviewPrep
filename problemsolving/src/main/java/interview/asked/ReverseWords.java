ad4a2516a79058190162a084f82f758db32d6778
manishkumarmdb <manishkumarmdb@gmail.com>
2026-02-10 00:58:02 -0600

    revised on reverse word in a given String array

--- problemsolving/src/main/java/interview/asked/ReverseWords.java
+++ problemsolving/src/main/java/interview/asked/ReverseWords.java
@@ -4,20 +4,20 @@ import java.util.Arrays;
 
 public class ReverseWords {
 
-    public static String[] reverseWord(String[] S) {
-        int left = 0;
-        int right = S.length - 1;
-        while (left < right) {
-            String leftStr = S[left];
-            S[left++] = S[right];
-            S[right--] = leftStr;
-        }
-
-        return S;
+  public static String[] reverseWord(String[] S) {
+    int left = 0;
+    int right = S.length - 1;
+    while (left < right) {
+      String leftStr = S[left];
+      S[left++] = S[right];
+      S[right--] = leftStr;
     }
 
-    public static void main(String[] args) {
-        String[] input = new String[] {"manish", "mani", "deepak", "push", "pop"};
-        System.out.println(Arrays.toString(reverseWord(input)));
-    }
+    return S;
+  }
+
+  public static void main(String[] args) {
+    String[] input = new String[]{"manish", "mani", "deepak", "push", "pop"};
+    System.out.println(Arrays.toString(reverseWord(input)));
+  }
 }
